81b43d31b200ad85d646f955b109cd10a077320d
Rodion Rasulov <psyrr4@nottingham.ac.uk>
2025-03-02 19:18:34 +0000

    CNN for PPO and hyperparameters fix

--- Mario/PPO/mario.py
+++ Mario/PPO/mario.py
@@ -8,56 +8,44 @@ import cv2
 from PIL import Image
 import torch
 from gym.wrappers import GrayScaleObservation, ResizeObservation, FrameStack, RecordEpisodeStatistics, RecordVideo
+from stable_baselines3.common.atari_wrappers import NoopResetEnv, MaxAndSkipEnv, EpisodicLifeEnv
 
 #handles the environment, pre-processing using wrappers and
 #overriding the default step and reset methods
 class Mario(gym.Wrapper):
     #rgb_array gives pixel info of game for us to work with
     # human mode actually allows to see
-    def __init__(self,repeat=4,device='cpu',env_id="SuperMarioBros-1-1-v0"):
+    def __init__(self,device='cpu',env_id="SuperMarioBros-1-1-v0"):
         env = gym_super_mario_bros.make(env_id)
         env = JoypadSpace(env, RIGHT_ONLY)
 
-        #apply wrappers for preprocessing of images
-        env = ResizeObservation(env,shape=84)
-        env = GrayScaleObservation(env)
-        env = FrameStack(env,num_stack=4,lz4_compress=True)
+        #aaply wrappers for statistics
         env = RecordEpisodeStatistics(env) #record statistics of episode return
         #env = RecordVideo(env,"videos", record_video_trigger=lambda t: t % 100 == 0) # record video of agent playing
+        
+        #take random number of NOOPs on reset. overwrite reset function, sample random number of noops between 0 and 30, execute the noop and then return
+        # add stochasticity to environment
+        env = NoopResetEnv(env=env,noop_max=30)
+        # skip 4 frames by default, repeat agents actions for those frames. Done for efficiency. Take the max pixel values over last 2 frames
+        env = MaxAndSkipEnv(env=env,skip=4)
+        # wrapper treats every end of life as end of that episode. So, if any life is lost episode ends. But reset is called only if lives are exhausted
+        env = EpisodicLifeEnv(env=env) # this one might not work as it expects ale, and others also expect terminated as part of "step" function. Can rewrite the wrapper though
+        #apply wrappers for preprocessing of images
+        env = ResizeObservation(env,(84,84)) # for efficiency
+        env = GrayScaleObservation(env)
+        env = FrameStack(env,num_stack=4,lz4_compress=True) #stack 4 past observations together as a single observation. Helps agent identify velocities of objects
 
         super(Mario,self).__init__(env)#parent class initialiser, gym wrapper
 
         self.action_num = len(RIGHT_ONLY)
         self.env = env
-        self.repeat = repeat
-        self.lives = 3
         self.device = device
-
-    #take action on an environment ->returns a state 
-    def step(self,action):
-        total_reward = 0.0
-        done = False
-        
-        #repeat same action for self.repeat steps as have high frame rate
-        for _ in range(self.repeat):
-            state,reward,done, info = self.env.step(action) # the reward function e.g. for mario, is defined within that 
-            #environment. In breakout, breaking a brick gives a reward
-            total_reward += reward 
-            if done:
-                break
-
-        #why return these?
-        #total_reward: feedback for learnng
-        #done: indicate if game finished so episode can end
-        return state,total_reward,done,info
 
     #2 functions we use the most -> reset and step
     #reset is to bring it back to setup state
     #overriding it here compared to default
     def reset(self):
         state = self.env.reset()
-
-        self.lives = 3
         #initial state
 
         return state
--- Mario/PPO/model.py
+++ Mario/PPO/model.py
@@ -4,9 +4,28 @@ import torch.nn as nn
 import numpy as np
 from torch.distributions.categorical import Categorical
 
+# Note: PPO is meant to be run primarily on the CPU, so dont put this on gpu
 class MarioNet(nn.Module):
-    def __init__(self,envs):
+    def __init__(self,envs,input_shape):
         super(MarioNet,self).__init__()
+        
+        self.cnn = nn.Sequential(
+            layer_init(nn.Conv2d(input_shape[0],32,8,stride=4)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(32,64,4,stride=2)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(64,64,3,stride=1)),
+            nn.ReLU(),
+            nn.Flatten(),
+            #linear layer that takes input of the flattened features
+            layer_init(nn.Linear(64*7*7, 512)), # get reduced into a 7x7 image with 64 channels 
+            # TODO: rewrite this to be better and programatical e.g. get_flat_size() function 
+            nn.ReLU(),
+        )
+        # TODO: may need more layers here, and more than 512
+        self.actor = layer_init(nn.Linear(512, envs.single_action.space.n), std=0.01)
+        self.critic = layer_init(nn.Linear(512,1), std=1)
+        """
         self.critic = nn.Sequential(
             layer_init(nn.Linear(np.array(envs.single_observation_space.shape).prod(),64)), #input shape to first layer is product of obesrvation space
             nn.Tanh(),
@@ -22,19 +41,23 @@ class MarioNet(nn.Module):
             nn.Tanh(),
             layer_init(nn.Linear(64,envs.single_action_space.n) , std=0.01),
         )
+        """
     
     def get_value(self,x):
-        return self.critic(x)
+        #divide by 255 -> the image observation has a range 0-255, we get it range of 0 to 1
+        return self.critic(self.cnn(x / 255.0)) #go through cnn first then critic
 
-    def get_action_plus_value(self,x,action=None):
-        logits = self.actor(x) #unnormalised action probabilities
+    def get_action_plus_value(self,x,action=None):  
+        #divide by 255 -> the image observation has a range 0-255, we get it range of 0 to 1
+        hidden = self.cnn(x / 255.0) #get the hidden layer output, after CNN input
+        logits = self.actor(hidden) #unnormalised action probabilities
         probabilities = Categorical(logits=logits) #softmax operation to get the action probability distribution we need
         if action is None:
             action = probabilities.sample
         #return actions, log probabilities, entropies and values from critic
-        return action,probabilities.log_prob(action), probabilities.entropy(),self.critic(x)
+        return action,probabilities.log_prob(action), probabilities.entropy(),self.critic(hidden)
 
-
+# TODO: go over this 
 def layer_init(layer, std=np.sqrt(2), bias_const=0.0): #use sqrt 2 as standard deviation
     torch.nn.init.orthogonal_(layer.weight,std)
     torch.nn.init.constant_(layer.bias,bias_const)
