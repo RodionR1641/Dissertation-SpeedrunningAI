13cb3d12d4cd6f8c1fa24ea5422b2c66d127be5c
Rodion Rasulov <psyrr4@nottingham.ac.uk>
2025-03-07 14:52:57 +0000

    RND in PPO, changed loss function

---
+++ Mario/PPO/model_RND.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+import torch
+import numpy as np
+
+class RNDNetwork(nn.Module):
+    def __init__(self, input_shape):
+        super(RNDNetwork, self).__init__()
+        self.cnn = nn.Sequential(
+            layer_init(nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(32, 64, kernel_size=4, stride=2)),
+            nn.ReLU(),
+            layer_init(nn.Conv2d(64, 64, kernel_size=3, stride=1)),
+            nn.ReLU(),
+            nn.Flatten(),
+            layer_init(nn.Linear(64 * 7 * 7, 512)),  # Same as MarioNet
+            nn.ReLU(),
+            layer_init(nn.Linear(512, 512)),  # Output feature vector
+        )
+
+    def forward(self, x):
+        return self.cnn(x)
+
+
+# TODO: go over this 
+def layer_init(layer, std=np.sqrt(2), bias_const=0.0): #use sqrt 2 as standard deviation
+    torch.nn.init.orthogonal_(layer.weight,std)
+    torch.nn.init.constant_(layer.bias,bias_const)
+    return layer
